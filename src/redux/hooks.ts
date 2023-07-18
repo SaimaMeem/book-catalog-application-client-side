cb76e01f443c483aa984d1d50c4e5d0fd39b449d
SaimaMeem <saima.sultana1320@gmail.com>
2023-07-18 06:46:33 +0600

    feat: setup redux with rtk query

---
+++ src/redux/hooks.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import type { TypedUseSelectorHook } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import type { AppDispatch, RootState } from './store';
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
