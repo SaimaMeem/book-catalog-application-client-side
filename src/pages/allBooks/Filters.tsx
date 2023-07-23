09151173686c31749c844fb426afcb8e5d5a6288
SaimaMeem <saima.sultana1320@gmail.com>
2023-07-23 22:23:54 +0600

    chore: make filter section responsive

--- src/pages/allBooks/Filters.tsx
+++ src/pages/allBooks/Filters.tsx
@@ -53,145 +53,169 @@ export default function Filters() {
     };
     return (
         <>
-            <aside className="sidebar-sticky sidebar h-screen justify-start bg-[#FCFCFC] shadow-[8px_0_15px_-4px_rgba(0,0,0,0.2)] ">
-                <section className="sidebar-content min-h-[20rem]">
-                    <nav className="menu rounded-md">
-                        <section className="menu-section px-4">
-                            <span className="menu-title text-xl font-bold">
-                                Filters
-                            </span>
-                            <ul className="menu-items">
-                                <li>
-                                    <input
-                                        type="checkbox"
-                                        id="menu-1"
-                                        className="menu-toggle"
-                                    />
-                                    <label
-                                        className="menu-item justify-between"
-                                        htmlFor="menu-1"
-                                    >
-                                        <div className="flex gap-2">
-                                            <span>Genre</span>
-                                        </div>
+            <div className="sm:w-full sm:max-w-[18rem]">
+                <input
+                    type="checkbox"
+                    id="sidebar-mobile-fixed"
+                    className="sidebar-state"
+                />
+                <label
+                    htmlFor="sidebar-mobile-fixed"
+                    className="sidebar-overlay"
+                ></label>
+                <div className="w-fit">
+                    <label
+                        htmlFor="sidebar-mobile-fixed"
+                        className="btn-primary btn sm:hidden"
+                    >
+                        Open Sidebar
+                    </label>
+                </div>
+                {/* <aside className="sidebar sidebar-fixed-left sidebar-mobile h-full justify-start max-sm:fixed max-sm:-translate-x-full bg-[#FCFCFC] shadow-[8px_0_15px_-4px_rgba(0,0,0,0.2)]"></aside> */}
+                <aside className="sidebar sidebar-sticky sidebar-mobile h-full justify-start max-sm:fixed max-sm:-translate-x-full bg-[#FCFCFC] shadow-[8px_0_15px_-4px_rgba(0,0,0,0.2)] w-auto">
+                    <section className="sidebar-content min-h-[20rem]">
+                        <nav className="menu rounded-md">
+                            <section className="menu-section px-4">
+                                <span className="menu-title text-xl font-bold">
+                                    Filters
+                                </span>
+                                <ul className="menu-items">
+                                    <li>
+                                        <input
+                                            type="checkbox"
+                                            id="menu-1"
+                                            className="menu-toggle"
+                                        />
+                                        <label
+                                            className="menu-item justify-between"
+                                            htmlFor="menu-1"
+                                        >
+                                            <div className="flex gap-2">
+                                                <span>Genre</span>
+                                            </div>
 
-                                        <span className="menu-icon">
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                className="h-5 w-5"
-                                                viewBox="0 0 20 20"
-                                                fill="currentColor"
-                                            >
-                                                <path
-                                                    fillRule="evenodd"
-                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                    clipRule="evenodd"
-                                                />
-                                            </svg>
-                                        </span>
-                                    </label>
-
-                                    <div
-                                        className="menu-item-collapse"
-                                        id="group1"
-                                    >
-                                        <div className="min-h-0">
-                                            {uniqueGenres.map((uniqueGenre) => (
-                                                <label
-                                                    key={uniqueGenre?.id}
-                                                    className={`flex cursor-pointer gap-2 menu-item ml-1 ${isCheckedGenre(
-                                                        uniqueGenre?.genre as string
-                                                    )}`}
+                                            <span className="menu-icon">
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    className="h-5 w-5"
+                                                    viewBox="0 0 20 20"
+                                                    fill="currentColor"
                                                 >
-                                                    <input
-                                                        type="checkbox"
-                                                        value={
-                                                            uniqueGenre?.genre as string
-                                                        }
-                                                        onChange={
-                                                            handleGenreFilter
-                                                        }
+                                                    <path
+                                                        fillRule="evenodd"
+                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                        clipRule="evenodd"
                                                     />
-                                                    <span>
-                                                        {
-                                                            uniqueGenre.genre as ReactNode
-                                                        }
-                                                    </span>
-                                                </label>
-                                            ))}
-                                        </div>
-                                    </div>
-                                </li>
-                                <li>
-                                    <input
-                                        type="checkbox"
-                                        id="menu-2"
-                                        className="menu-toggle"
-                                    />
-                                    <label
-                                        className="menu-item justify-between"
-                                        htmlFor="menu-2"
-                                    >
-                                        <div className="flex gap-2">
-                                            <span>Publication Year</span>
-                                        </div>
+                                                </svg>
+                                            </span>
+                                        </label>
 
-                                        <span className="menu-icon">
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                className="h-5 w-5"
-                                                viewBox="0 0 20 20"
-                                                fill="currentColor"
-                                            >
-                                                <path
-                                                    fillRule="evenodd"
-                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                    clipRule="evenodd"
-                                                />
-                                            </svg>
-                                        </span>
-                                    </label>
+                                        <div
+                                            className="menu-item-collapse"
+                                            id="group1"
+                                        >
+                                            <div className="min-h-0">
+                                                {uniqueGenres.map(
+                                                    (uniqueGenre) => (
+                                                        <label
+                                                            key={
+                                                                uniqueGenre?.id
+                                                            }
+                                                            className={`flex cursor-pointer gap-2 menu-item ml-1 ${isCheckedGenre(
+                                                                uniqueGenre?.genre as string
+                                                            )}`}
+                                                        >
+                                                            <input
+                                                                type="checkbox"
+                                                                value={
+                                                                    uniqueGenre?.genre as string
+                                                                }
+                                                                onChange={
+                                                                    handleGenreFilter
+                                                                }
+                                                            />
+                                                            <span>
+                                                                {
+                                                                    uniqueGenre.genre as ReactNode
+                                                                }
+                                                            </span>
+                                                        </label>
+                                                    )
+                                                )}
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <input
+                                            type="checkbox"
+                                            id="menu-2"
+                                            className="menu-toggle"
+                                        />
+                                        <label
+                                            className="menu-item justify-between"
+                                            htmlFor="menu-2"
+                                        >
+                                            <div className="flex gap-2">
+                                                <span>Publication Year</span>
+                                            </div>
 
-                                    <div
-                                        className="menu-item-collapse"
-                                        id="group2"
-                                    >
-                                        <div className="min-h-0">
-                                            {uniquePublicationYears.map(
-                                                (uniquePublicationYear) => (
-                                                    <label
-                                                        key={
-                                                            uniquePublicationYear?.id
-                                                        }
-                                                        className={`flex cursor-pointer gap-2 menu-item ml-1 ${isCheckedPublicationYear(
-                                                            uniquePublicationYear?.publicationYear as string
-                                                        )}`}
-                                                    >
-                                                        <input
-                                                            type="checkbox"
-                                                            value={
+                                            <span className="menu-icon">
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    className="h-5 w-5"
+                                                    viewBox="0 0 20 20"
+                                                    fill="currentColor"
+                                                >
+                                                    <path
+                                                        fillRule="evenodd"
+                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                        clipRule="evenodd"
+                                                    />
+                                                </svg>
+                                            </span>
+                                        </label>
+
+                                        <div
+                                            className="menu-item-collapse"
+                                            id="group2"
+                                        >
+                                            <div className="min-h-0">
+                                                {uniquePublicationYears.map(
+                                                    (uniquePublicationYear) => (
+                                                        <label
+                                                            key={
+                                                                uniquePublicationYear?.id
+                                                            }
+                                                            className={`flex cursor-pointer gap-2 menu-item ml-1 ${isCheckedPublicationYear(
                                                                 uniquePublicationYear?.publicationYear as string
-                                                            }
-                                                            onChange={
-                                                                handlePublicationYearFilter
-                                                            }
-                                                        />
-                                                        <span>
-                                                            {
-                                                                uniquePublicationYear.publicationYear as ReactNode
-                                                            }
-                                                        </span>
-                                                    </label>
-                                                )
-                                            )}
+                                                            )}`}
+                                                        >
+                                                            <input
+                                                                type="checkbox"
+                                                                value={
+                                                                    uniquePublicationYear?.publicationYear as string
+                                                                }
+                                                                onChange={
+                                                                    handlePublicationYearFilter
+                                                                }
+                                                            />
+                                                            <span>
+                                                                {
+                                                                    uniquePublicationYear.publicationYear as ReactNode
+                                                                }
+                                                            </span>
+                                                        </label>
+                                                    )
+                                                )}
+                                            </div>
                                         </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </section>
-                    </nav>
-                </section>
-            </aside>
+                                    </li>
+                                </ul>
+                            </section>
+                        </nav>
+                    </section>
+                </aside>
+            </div>
         </>
     );
 }
